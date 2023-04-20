4072fd4ff341b5f15917dcc1b1ac23a53c5a418e
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2023-04-19 21:19:04 -0300

    base: splash screen

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -12,6 +12,17 @@ export default function Splash() {
           <S.Card2 />
           <S.Card3 />
         </S.Cards>
+        <S.Texts>
+          <S.Title>Easy Way to {'\n'}Save Your Money</S.Title>
+          <S.Description>
+            The best place to transact and save {'\n'}money
+          </S.Description>
+        </S.Texts>
+        <S.Button>
+          <S.ButtonGradient>
+            <S.ButtonText>Get Started</S.ButtonText>
+          </S.ButtonGradient>
+        </S.Button>
       </S.Background>
     </S.Container>
   );
--- src/screens/splash/styles.ts
+++ src/screens/splash/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import * as Native from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const pixelRatio = Native.PixelRatio.get();
 
@@ -27,51 +28,93 @@ export const Logo = styled(AtomBank)`
   width: ${pixelRatio * 157}px;
   height: ${pixelRatio * 59}px;
 
-  margin: 66px auto 0 auto;
+  margin: 60px auto 0 auto;
 `;
 
 export const Cards = styled(Native.View)`
-  flex-direction: row;
+  flex: 1;
 
-  width: 100%;
+  flex-direction: row;
 
   justify-content: center;
 
-  position: relative;
-
-  top: 50px;
+  margin-top: 30px;
 `;
 
 export const Card1 = styled(Card_1)`
-  position: absolute;
-
-  left: -80px;
-
   z-index: 3;
 
-  transform: rotateX(-20deg) rotateY(-40deg) rotateZ(20deg);
+  margin-right: -100px;
+
+  transform: rotateX(-30deg) rotateY(-20deg) rotateZ(10deg);
   transform-style: preserve-3d;
 `;
 
 export const Card2 = styled(Card_2)`
-  position: absolute;
-
   z-index: 2;
 
-  top: 100px;
+  margin-top: 100px;
 
-  transform: rotateX(-10deg) rotateY(-40deg) rotateZ(-00deg);
+  transform: rotateX(-20deg) rotateY(30deg) rotateZ(-10deg);
   transform-style: preserve-3d;
 `;
 
 export const Card3 = styled(Card_3)`
-  position: absolute;
-
-  right: -100px;
-  top: 200px;
-
   z-index: 1;
 
-  transform: rotateX(-10deg) rotateY(-40deg) rotateZ(20deg);
+  margin-top: 200px;
+  margin-left: -130px;
+
+  transform: rotateX(-20deg) rotateY(-20deg) rotateZ(10deg);
   transform-style: preserve-3d;
+`;
+
+export const Texts = styled(Native.View)`
+  width: 90%;
+
+  margin: 0 auto;
+`;
+
+export const Title = styled(Native.Text)`
+  font-family: 'Poppins-Bold';
+  font-size: 36px;
+
+  color: #eff1f3;
+`;
+
+export const Description = styled(Native.Text)`
+  font-family: 'Montserrat';
+  font-size: 12px;
+
+  color: #989898;
+`;
+
+export const Button = styled(Native.TouchableOpacity)`
+  width: 90%;
+  height: 72px;
+
+  justify-content: center;
+
+  margin: 20px auto 20px auto;
+`;
+
+export const ButtonGradient = styled(LinearGradient).attrs({
+  start: {x: 0.0, y: 0.25},
+  end: {x: 0.5, y: 1.0},
+  locations: [0.1, 1],
+  colors: ['#ADE446', '#13C93E'],
+})`
+  height: 72px;
+
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 32px;
+`;
+
+export const ButtonText = styled(Native.Text)`
+  font-family: 'Poppins-Bold';
+  font-size: 16px;
+
+  color: #ffffff;
 `;
